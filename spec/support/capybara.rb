cdccc370479b1bd9eb2a43a895f4033b80ef9bae
shgtkshruch <shgtk.shruch@gmail.com>
2019-08-24 00:45:19 +0900

    Add webdirver options

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -7,3 +7,12 @@ RSpec.configure do |config|
     driven_by :selenium_chrome_headless
   end
 end
+
+Capybara.register_driver :selenium_chrome_headless do |app|
+  options = ::Selenium::WebDriver::Chrome::Options.new
+  options.add_argument('--headless')
+  options.add_argument('--no-sandbox')
+  options.add_argument('--disable-dev-shm-usage')
+  options.add_argument('--window-size=1400,1400')
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+end

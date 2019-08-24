fbf4a7e1460ce151b490cf9a4b13f76930c91132
shgtkshruch <shgtk.shruch@gmail.com>
2019-08-24 11:36:10 +0900

    Refactoring note spec using describe and context

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -1,62 +1,72 @@
 require 'rails_helper'
 
 RSpec.describe Note, type: :model do
-  # 検索文字列に一致するメモを返すこと
-  it "returns notes that match the search term" do
-    user = User.create(
-      first_name: "Aaron",
-      last_name:  "Sumner",
-      email:      "tester@example.com",
-      password:   "dottle-nouveau-pavilion-tights-furze"
-    )
+  # 文字列に一致するメッセージを検索する
+  describe "search message for a term" do
 
-    project = user.projects.create(name: "Test Project")
+    # 一致するデータが見つかるとき
+    context "when a match is found" do
+      # 検索文字列に一致するメモを返すこと
+      it "returns notes that match the search term" do
+        user = User.create(
+          first_name: "Aaron",
+          last_name:  "Sumner",
+          email:      "tester@example.com",
+          password:   "dottle-nouveau-pavilion-tights-furze"
+        )
 
-    note1 = project.notes.create(
-      message: 'This is the first note.',
-      user: user
-    )
+        project = user.projects.create(name: "Test Project")
 
-    note2 = project.notes.create(
-      message: 'This is the second note.',
-      user: user
-    )
+        note1 = project.notes.create(
+          message: 'This is the first note.',
+          user: user
+        )
 
-    note3 = project.notes.create(
-      message: 'First, preheat the oven..',
-      user: user
-    )
+        note2 = project.notes.create(
+          message: 'This is the second note.',
+          user: user
+        )
 
-    expect(Note.search("first")).to include(note1, note3)
-    expect(Note.search("first")).to_not include(note2)
-  end
+        note3 = project.notes.create(
+          message: 'First, preheat the oven..',
+          user: user
+        )
 
-  # 検索結果が1件も見つからなければ空のコレクションを返すこと
-  it "returns an empty collection when no results are found" do
-    user = User.create(
-      first_name: "Aaron",
-      last_name:  "Sumner",
-      email:      "tester@example.com",
-      password:   "dottle-nouveau-pavilion-tights-furze"
-    )
+        expect(Note.search("first")).to include(note1, note3)
+        expect(Note.search("first")).to_not include(note2)
+      end
+    end
 
-    project = user.projects.create(name: "Test Project")
+    # 一致するデータが1件も見つからないとき
+    context "when no match is found" do
+      # 検索結果が1件も見つからなければ空のコレクションを返すこと
+      it "returns an empty collection when no results are found" do
+        user = User.create(
+          first_name: "Aaron",
+          last_name:  "Sumner",
+          email:      "tester@example.com",
+          password:   "dottle-nouveau-pavilion-tights-furze"
+        )
 
-    note1 = project.notes.create(
-      message: 'This is the first note.',
-      user: user
-    )
+        project = user.projects.create(name: "Test Project")
 
-    note2 = project.notes.create(
-      message: 'This is the second note.',
-      user: user
-    )
+        note1 = project.notes.create(
+          message: 'This is the first note.',
+          user: user
+        )
 
-    note3 = project.notes.create(
-      message: 'First, preheat the oven..',
-      user: user
-    )
+        note2 = project.notes.create(
+          message: 'This is the second note.',
+          user: user
+        )
 
-    expect(Note.search("tamarb")).to be_empty
+        note3 = project.notes.create(
+          message: 'First, preheat the oven..',
+          user: user
+        )
+
+        expect(Note.search("tamarb")).to be_empty
+      end
+    end
   end
 end
